bf74243db949a48edfe834fbda79d90cb184982f
josesouza <joseluisdesouza9492@gmail.com>
2022-04-13 14:49:33 -0300

    start project

---
+++ src/main/java/com/api/automotivefactory/AutomotiveFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.api.automotivefactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomotiveFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomotiveFactoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/automotivefactory/AutomotiveFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.automotivefactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomotiveFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
